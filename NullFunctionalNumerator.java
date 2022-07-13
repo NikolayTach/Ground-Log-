6c686e33ba861feb5a374c5261d7e81646232da2
NikolayTach <42199859+NikolayTach@users.noreply.github.com>
2022-07-12 17:13:23 -0700

    Add files via upload

---
+++ NullFunctionalNumerator.java
@@ -0,0 +1,7 @@
+	{public ClassUnit(0) assert NullFunctionalNumerator($5)
+	}
+{
+class import log_ground(0) default ReaderFocusHOLD 
+import log_ground(0) default ReaderFocusHOLD
+import log_ground(0) to ClassUnit(0) than assert Flushable SubterminalLogDistance(1)
+}
