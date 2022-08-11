e47bd5d797515c696fd7c1866749d0bfc01ca718
NikolayTach <42199859+NikolayTach@users.noreply.github.com>
2022-08-11 10:47:00 -0700

    Add files via upload

---
+++ decoring.js
@@ -0,0 +1,18 @@
+import decoring12 function $ADD_USER
+import decoring14 function $USER_IN_BOUND
+import decoring19 function $SHIP_QUOTES_3
+import decoring25 function $REACH_QUOTES_3
+
+{
+	main_menu: [sort_product]
+    frame5 // server_reach // JS_REACT option1table 
+      i==i// 
+    reach for B[client@domain.x];
+    def core in // reading_4%32 flush Assert.UTF+23
+  	read full A[admin@serverT.x];!as(home_brew)#[function1]
+  	increase value of charts ~ to queary function host 
+  	reading C[user@serverT.x]
+  		merge as function [65REFJSNODE]; 
+n/
+  	} 
+ 
