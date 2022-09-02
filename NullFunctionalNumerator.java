35e24479cc6b3b9257143f4d834f84630ccacaf4
NikolayTach <42199859+NikolayTach@users.noreply.github.com>
2022-09-02 08:07:31 -0700

    Delete NullFunctionalNumerator.java

--- NullFunctionalNumerator.java
+++
@@ -1,7 +0,0 @@
-	{public ClassUnit(0) assert NullFunctionalNumerator($5)
-	}
-{
-class import log_ground(0) default ReaderFocusHOLD 
-import log_ground(0) default ReaderFocusHOLD
-import log_ground(0) to ClassUnit(0) than assert Flushable SubterminalLogDistance(1)
-}
