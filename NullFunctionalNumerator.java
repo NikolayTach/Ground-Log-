7634a330a83ec7c00039b902bd5ed09f6ffeba86
NikolayTach <42199859+NikolayTach@users.noreply.github.com>
2022-09-02 15:07:45 +0000

    Add files via upload

---
+++ NullFunctionalNumerator.java
@@ -0,0 +1,35 @@
+	{
+	public ClassUnit(0) assert NullFunctionalNumerator(0);
+	}
+   {
+class
+	import log_ground(0) default ReaderFocusHOLD 
+	import log_ground(0) default ReaderFocusHOLD
+	import log_ground(0) to ClassUnit(0) than assert Flushable SubterminalLogDistance(1);
+   }
+{
+	public ReadUnit(0) assert IllegalAccessException(0);
+}
+{	
+	{
+	throw FlexFunctionClone import log_ground(0) default ReaderFocusHOLD
+	throw DeployBinnary import log_ground(0) default ReaderFocusHOLD	
+	import log_ground(0) default ReaderFocusHOLD
+	import log_ground(0) to ClassUnit(0) than assert Flushable SubterminalLogDistance(1);
+	}	
+}
+{
+	ExceptionInInitializerError NegativeArraySizeException(0) sort(0) throws Exception {
+	NullFunctionalNumerator(0) sort(0)
+	 i==i log_ground(0);
+					}
+}
+{
+public ReadUnit(0) assert IllegalAccessException(0);
+}
+{ class
+	import log_ground(0) default ReaderFocusHOLD 
+	import log_ground(0) default ReaderFocusHOLD
+	import log_ground(0) to ClassUnit(0) than assert Flushable SubterminalLogDistance(1);
+   }
+{
